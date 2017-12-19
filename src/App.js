634cc66ae96f7b1ef4fdbc7e9a15f542b2d5a85c
voronko.slava <unglory44@gmail.com>
2017-12-19 05:03:33 +0300

    Added user avatar

--- src/App.js
+++ src/App.js
@@ -1,301 +1,324 @@
 import React, { Component } from 'react'; 
-import { FlatButton, AppBar, TextField, 
-Dialog, Tabs, Tab } 
-from 'material-ui';
+import { 
+		FlatButton, 
+		AppBar, 
+		TextField, Dialog, 
+		Tabs, 
+		Tab, 
+		Avatar,
+		IconMenu,
+		MenuItem,
+		IconButton
+		} from 'material-ui';
 import firebase from 'firebase';
 import 'firebase/firestore';
-import { blue300, blue500, blue700 } from 
-'material-ui/styles/colors';
+import { blue300, blue500, blue700 } from 'material-ui/styles/colors';
 
 import './App.css';
 
 const styles = {
-  appBarStyle: {
-    backgroundColor: blue500,
-  },
-  primaryStyle: {
-    backgroundColor: blue500,
-    color: '#fff',
-  },
-  dialogStyle: {
-    height: '400px',
-    maxHeight: 'none',
-  },
-  inputStyle: {
-    width: '100%',
-    maxWidth: '100%',
-  },
+	appBarStyle: {
+		backgroundColor: blue500,
+	},
+	primaryStyle: {
+		backgroundColor: blue500,
+		color: '#fff',
+	},
+	dialogStyle: {
+		height: '400px',
+		maxHeight: 'none',
+	},
+	inputStyle: {
+		display: 'block',
+		width: '100%',
+		maxWidth: '300px',
+		margin: '0 auto 20px',
+	},
 };
 
 
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      test: 'Test Value',
-      authForm: {
-        login: 'login',
-	pswd: 'pswd',
-      },
-      authTabsVal: 'login',
-      authDialogOpen: false,
-      authCheck: false,
-    }
-  }
+	constructor() {
+		super();
+		this.state = {
+			test: 'Test Value',
+			authForm: {
+				login: 'login',
+				pswd: 'pswd',
+			},
+			authTabsVal: 'login',
+			authDialogOpen: false,
+			authCheck: false,
+			userEmail: '',
+		}
+	}
 
-  componentWillMount() {
-    const fire = firebase.auth();
+	componentWillMount() {
+		firebase.auth().onAuthStateChanged((user) => {
+		  if (user) {
+		    console.log( user.email );
+		    this.setState({
+		    	authCheck: true,
+		    	userEmail: user.email,
+		    });
+		  } else {
+		    console.log( 'Empty user' );
+		    this.setState({
+		    	authCheck: false,
+		    });
+		  }
+		});
 
-  }
+	}
 
-  componentDidMount() {
-   const fire = firebase.auth();
+	componentDidMount() {
 
-   if (fire.currentUser) {
-    alert(fire.currentUser.email)
-   } else {
-    alert('Авторизуйтесь')
-
-   }
-
-  }
-
-
-  handleLoginChange (e) {
-    this.setState({
-      authForm: {
-        login: e.target.value,
-	pswd: this.state.authForm.pswd
-      }
-    })
-  }
-
-  handlePswdChange (e) {
-    this.setState({
-      authForm: {
-        login: this.state.authForm.login,
-	pswd: e.target.value
-      }
-    })
-  }
+	}
 
 
-  handleAuthLogin() {
-   const email = this.state.authForm.login,
-        pswd = this.state.authForm.pswd,
-	fire = firebase.auth();
+	handleLoginChange (e) {
+		this.setState({
+			authForm: {
+				login: e.target.value,
+				pswd: this.state.authForm.pswd
+			}
+		})
+	}
+
+	handlePswdChange (e) {
+		this.setState({
+			authForm: {
+				login: this.state.authForm.login,
+				pswd: e.target.value
+			}
+		})
+	}
 
 
-fire.setPersistence(firebase.auth.Auth.Persistence.SESSION)
-  .then(() =>  {
-
-    alert('Login request');
-    fire.signInWithEmailAndPassword(email, pswd)
-
-    this.setState({
-     authCheck: true,
-     authDialogOpen: false,
-    });
-
-  })
-  .catch(error => {
-   alert(error.message)
-  });
+	handleAuthLogin() {
+		const email = this.state.authForm.login,
+					pswd = this.state.authForm.pswd,
+					fire = firebase.auth();
 
 
+		fire.setPersistence(firebase.auth.Auth.Persistence.SESSION)
+			.then(() =>  {
 
-  }
+			alert('Login request');
+			fire.signInWithEmailAndPassword(email, pswd)
 
- handleSignOut() {
-  firebase.auth().signOut().then(function() {
-    alert('SignOut success')
-  });
+			this.setState({
+			 authCheck: true,
+			 authDialogOpen: false,
+			});
 
- this.setState({
-  authCheck: !this.state.authCheck,
- });
-
- }
-
-
-
-  handleAuthSubmit(e) {
-
-  const email = this.state.authForm.login,
-        pswd = this.state.authForm.pswd;
-
-firebase.auth().createUserWithEmailAndPassword(email, pswd).catch(function(err) {
-    alert(err.message)
-   });
-  }
-
-  showAuthForm() {
-   return (
-    <TextField
-     style={styles.inputStyle}
-     hintText='Логин'
-    />
-   )
-  }
-
-  handleAuthDialogOpen = () =>  {
-    this.setState({
-      authDialogOpen: !this.state.authDialogOpen
-     }) 
-  }
-
-  handleLoginWrite(e) {
-    this.setState({
-     authForm: {
-      login: e.target.value,
-      pswd: this.state.authForm.pswd,
-     },
-    });
-  }
-
-  handlePswdWrite(e) {
-    this.setState({
-     authForm: {
-      login: this.state.authForm.login,
-      pswd: e.target.value,
-     },
-    });
-  }
-
-  handleAuthTabsChange(val) {
-   this.setState({
-    authTabsVal: val,
-   });
-  }
+		})
+		.catch(error => {
+		 alert(error.message)
+		});
 
 
-
-  render() {
-
-  const dialogLoginBtn = [
-    <FlatButton
-      label='Войти'
-      onClick={
-       () => this.handleAuthLogin()}
-      style={styles.primaryStyle}
-    />
-  ];
-  const dialogRegBtn = [
-    <FlatButton
-     label='Зарегистрироваться'
-     onClick={
-      () => this.handleAuthSubmit()
-     }
-     style={styles.primaryStyle}
-    />
-  ];
+	}
 
 
-    return (
-      <div className="App">
-	<AppBar
-	  title="Чат"
-	  titleStyle={{fontSize:15,}}
-	  style={styles.appBarStyle}
-	  iconElementRight={
-	    this.state.authCheck ?
-	     <FlatButton
-	      label='Выход'
-	      style={{ fontSize: 15, }}
-	      onClick={
-	       () => this.handleSignOut()
-	      }
-	     />
-	    :
-	  <FlatButton 
-	    label="Войти"
-	    style={{fontSize:15,}}
-	    onClick={() =>this.handleAuthDialogOpen()}
-	  />}
-	/>
+	handleSignOut() {
+		firebase.auth().signOut().then(function() {
+			alert('SignOut success')
+		});
+
+		this.setState({
+			authCheck: !this.state.authCheck,
+		});
+
+	}
 
 
-        <br/>
-	<p>
-	 Current user: <br/> 
-	</p>
+	handleAuthSubmit(e) {
 
+		const email = this.state.authForm.login,
+					pswd = this.state.authForm.pswd;
 
-	<Dialog
-	  title={false}
-	  actions={
-	    dialogLoginBtn
-	  }
-	  modal={false}
-	  open={this.state.authDialogOpen}
-	  onRequestClose={
-	   this.handleAuthDialogOpen
-	  }
-	  bodyStyle={{
-	    //height: '40vh',
-	    minHeight: '50vh',
-	    padding: '0',
-	  }}
-	  contentStyle={{
-	    width: '94%',
-	    maxWidth: 'none',
-	  }}
-	>
-          <Tabs 
-	   value={this.state.authTabsVal}
-	   onChange={
-	    val => this.handleAuthTabsChange(val) }
-	   inkBarStyle={{ 
-	    backgroundColor: '#fff',
-	    minHeight: '6px'
-	   }}
-	   contentContainerStyle={{
-	    padding: '15px'
-	   }}
-	  >
-	    <Tab
-	     value='login'
-	     label='Вход'
-	     buttonStyle={{
-	      backgroundColor: blue500,
-	     }}
-	    >
-	    <TextField
-	      style={styles.inputStyle}
-	      hintText='Email'
-	      onChange={
-	      e =>this.handleLoginWrite(e)}
-	    />
+		firebase.auth().createUserWithEmailAndPassword(email, pswd).catch(function(err) {
+			alert(err.message)
+		});
+	}
 
-	    <TextField
-	      style={styles.inputStyle}
-	      hintText='Пароль'
-	      onChange={
-	      e => this.handlePswdWrite(e)}
-	    />
-	   </Tab>
+	showAuthForm() {
+	 return (
+		<TextField
+		 style={styles.inputStyle}
+		 hintText='Логин'
+		/>
+	 )
+	}
 
-	   <Tab
-	    value='reg'
-	    label='Регистрация'
-	    buttonStyle={{
-	     backgroundColor: blue500
-	    }}
-	   >
-	    sss
-	   </Tab>
+	handleAuthDialogOpen = () =>  {
+		this.setState({
+			authDialogOpen: !this.state.authDialogOpen
+		}) 
+	}
 
-          </Tabs>
+	handleLoginWrite(e) {
+		this.setState({
+			authForm: {
+				login: e.target.value,
+				pswd: this.state.authForm.pswd,
+		 	},
+		});
+	}
 
+	handlePswdWrite(e) {
+		this.setState({
+			authForm: {
+				login: this.state.authForm.login,
+				pswd: e.target.value,
+			},
+		});
+	}
 
-	</Dialog>
+	handleAuthTabsChange(val) {
+		this.setState({
+			authTabsVal: val,
+	 	});
+	}
 
 
 
-      </div>
-    );
-  }
+	render() {
+
+	const dialogLoginBtn = [
+		<FlatButton
+			label='Войти'
+			onClick={
+			 () => this.handleAuthLogin()}
+			style={styles.primaryStyle}
+		/>
+	];
+
+	const dialogRegBtn = [
+		<FlatButton
+		 label='Зарегистрироваться'
+		 onClick={
+			() => this.handleAuthSubmit()
+		 }
+		 style={styles.primaryStyle}
+		/>
+	];
+
+
+	
+
+
+		return (
+			<div className="app">
+				<AppBar
+					title="Чат"
+					titleStyle={{fontSize:15,}}
+					style={styles.appBarStyle}
+					iconElementRight={
+						this.state.authCheck
+						?
+			 			<Avatar
+							// label='Выход'
+							style={{ 
+								fontSize: 15, 
+								textTransform: 'uppercase', 
+								fontWeight: '300',
+								letterSpacing: '1px',
+								backgroundColor: '#FFC400',
+								cursor: 'pointer',
+								marginTop: '4px',
+								marginRight: '8px',
+							}}
+							onClick={
+							 () => this.handleSignOut()
+							}
+			 			>{ this.state.userEmail.substring(0,2) }</Avatar>
+						:
+						<FlatButton 
+							label="Войти"
+							style={{fontSize:15,}}
+							onClick={() =>this.handleAuthDialogOpen()}
+						/>}
+				/>
+
+				<div className="messages-container">
+
+				</div>
+
+
+				<Dialog
+					title={false}
+					actions={
+						dialogLoginBtn
+					}
+					modal={false}
+					open={this.state.authDialogOpen}
+					onRequestClose={
+					 this.handleAuthDialogOpen
+					}
+					bodyStyle={{
+						//height: '40vh',
+						minHeight: '50vh',
+						padding: 0,
+					}}
+					contentStyle={{
+						width: '94%',
+						maxWidth: '500px',
+					}}
+				>
+					<Tabs 
+						value={this.state.authTabsVal}
+						onChange={
+							val => this.handleAuthTabsChange(val) }
+						inkBarStyle={{ 
+							backgroundColor: '#fff',
+							minHeight: '6px'
+						}}
+						contentContainerStyle={{
+							padding: '15px'
+					 	}}
+					>
+						<Tab
+							value='login'
+							label='Вход'
+							buttonStyle={{
+								backgroundColor: blue500,
+							}}
+						>
+							<TextField
+								style={styles.inputStyle}
+								hintText='Email'
+								onChange={
+								e =>this.handleLoginWrite(e)}
+							/>
+
+							<TextField
+								style={styles.inputStyle}
+								hintText='Пароль'
+								type="password"
+								onChange={
+								e => this.handlePswdWrite(e)}
+							/>
+						</Tab>
+
+						<Tab
+							value='reg'
+							label='Регистрация'
+							buttonStyle={{
+								backgroundColor: blue500
+							}}
+					 	>
+							sss
+						</Tab>
+					</Tabs>
+				</Dialog>
+
+
+			</div>
+		);
+	}
 }
 
 export default App;
